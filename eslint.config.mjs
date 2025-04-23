3d0947cc050fbdd23a98739062a92619db69fa8c
Juan Antonio Caballero <j.antoniocaballero@hotmail.es>
2025-04-23 16:49:17 +0200

    fix eslint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,33 @@
 import globals from "globals";
 import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
-
+import parser from "@babel/eslint-parser";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: globals.browser } },
-  pluginReact.configs.flat.recommended,
+  {
+    files: ["**/*.{js,jsx}"],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "module",
+      parser,
+      parserOptions: {
+        requireConfigFile: false,
+        babelOptions: {
+          presets: ["@babel/preset-react"],
+        },
+      },
+      globals: globals.browser,
+    },
+    plugins: {
+      react: pluginReact,
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off",
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
 ]);
