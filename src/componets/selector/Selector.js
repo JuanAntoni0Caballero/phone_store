a3b4de61a320993f806d519545f0f66fc9b8f002
Juan Antonio Caballero <j.antoniocaballero@hotmail.es>
2025-04-24 19:41:43 +0200

    Created a test for selector component

--- src/componets/selector/Selector.js
+++ src/componets/selector/Selector.js
@@ -1,10 +1,13 @@
+// Selector.js
 import "./Selector.css";
 
 export default function Selector({ label, options, value, onChange }) {
+    const id = `selector-${label}`;
+
     return (
         <div className="selector">
-            <label className="selector-label">{label}:</label>
-            <select className="selector-select" value={value} onChange={(e) => onChange(e.target.value)}>
+            <label className="selector-label" htmlFor={id}>{label}</label>
+            <select id={id} className="selector-select" value={value} onChange={(e) => onChange(e.target.value)}>
                 {options.map((option) => (
                     <option key={option.code} value={option.code}>
                         {option.name}
